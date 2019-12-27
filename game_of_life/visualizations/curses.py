c4390e1d313c54983b5d27b5075423d364f684b2
Mike Conigliaro <mike@conigliaro.org>
2019-12-27 15:00:30 -0700

    Improve curses visualization

--- game_of_life/visualizations/curses.py
+++ game_of_life/visualizations/curses.py
@@ -1,14 +1,16 @@
-from curses import curs_set, wrapper
+import curses
 from game_of_life import grid, generations
 from time import sleep
 
 
 def main(stdscr, height, width, delay):
-    max_height, max_width = stdscr.getmaxyx()
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
     if not height:
-        height = max_height - 1
+        height = curses.LINES - 2
     if not width:
-        width = max_width - 1
+        width = curses.COLS - 1
 
     for i, g in enumerate(generations(grid(height, width))):
         population = 0
@@ -16,14 +18,23 @@ def main(stdscr, height, width, delay):
             for y in range(width):
                 if g[x][y] == 1:
                     population += 1
-                stdscr.addch(x, y, str(g[x][y]))
+                    stdscr.addch(x, y, '█')
+                else:
+                    stdscr.addch(x, y, ' ')
 
-        stdscr.addstr(max_height - 1, 0,
-                      f'Generation: {i}  Population: {population}')
-        curs_set(0)
+        status_bar = f'Ctrl+C to quit | '
+        status_bar += f'Generation: {i} | '
+        cells = height * width
+        pop_pct = f'{population / cells * 100 :.1f}'
+        status_bar += f'Population: {population}/{cells} ({pop_pct}%) | '
+        status_bar += f'Delay: {delay}s'
+        status_bar = status_bar.ljust(width, ' ')[:width]
+        stdscr.addstr(height + 1, 0, status_bar, curses.color_pair(1))
+
+        curses.curs_set(0)
         stdscr.refresh()
         sleep(delay)
 
 
 def run(height=None, width=None, delay=0):
-    wrapper(main, height=height, width=width, delay=delay)
+    curses.wrapper(main, height=height, width=width, delay=delay)
