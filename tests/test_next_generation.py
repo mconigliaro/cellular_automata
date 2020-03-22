8d93fbd0317c80b5f2f764958e7253744aa4fe78
Mike Conigliaro <mike@conigliaro.org>
2020-03-21 23:04:55 -0600

    Update tests

--- tests/test_next_generation.py
+++ tests/test_next_generation.py
@@ -51,5 +51,6 @@ def test_next_generation(grids, neighborhood):
     for grid1, grid2 in grids:
         rules = ca._parse_rulestring('b3/s23')
         grid1 = np.array(grid1)
-        next_gen = ca._next_generation(grid1, rules, neighborhood).grid.tolist()
+        next_gen = ca._next_generation(
+            grid1, rules, neighborhood).grid.tolist()
         assert next_gen == grid2
