43241d5ae4c2860234c8a12c730c15084b5f7e92
Mike Conigliaro <mike@conigliaro.org>
2020-04-16 02:09:29 -0600

    Show performance for "none" visualization

--- cellular_automata/visualizations/none.py
+++ cellular_automata/visualizations/none.py
@@ -1,3 +1,12 @@
+import time as t
+
+
 def run(generations, args):
-    for gen in generations:
-        pass
+    last_status = 0
+    for i, gen in enumerate(generations):
+        if t.time() - last_status > 1:
+            g = f'{i} ({1 / gen.time :.1f}/s)'
+            p_pct = f'{gen.population / gen.grid.size * 100 :.1f}'
+            p = f'{gen.population} ({p_pct}%)'
+            print(f'Generation: {g} | Population: {p}')
+            last_status = t.time()
